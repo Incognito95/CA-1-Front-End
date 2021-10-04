7b15c801b3d618777636adb4019aad92cbe32564
incognito95 <danielweisspedersen95@hotmail.com>
2021-10-04 15:30:04 +0200

    cleaned up startcode

---
+++ src/main/java/dtos/PersonDTO.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtos;
+
+import entities.Person;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author tha
+ */
+public class PersonDTO {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public PersonDTO(long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    
+
+
+
+    public PersonDTO() {
+       
+    }
+    
+    public PersonDTO(Person rm) {
+        if(rm.getId() != null)
+            this.id = rm.getId();
+        this.firstName = rm.getFirstName();
+        this.lastName = rm.getLastName();
+        this.email = rm.getEmail();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    public static List<PersonDTO> getDtos(List<Person> rms){
+        List<PersonDTO> rmdtos = new ArrayList();
+        rms.forEach(rm->rmdtos.add(new PersonDTO(rm)));
+        return rmdtos;
+    }
+}
