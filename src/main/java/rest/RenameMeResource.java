d2a2992f1e0e4029bae355fa8aceec87dc1b8ac7
incognito95 <danielweisspedersen95@hotmail.com>
2021-10-06 10:42:44 +0200

    Squashed commit of the following:
    commit 33dba6e2caefa81b685c7510f303f48cfb9f5326
    Author: Christoffer Quakernaat <cph-cq2@cphbusiness.dk>
    Date:   Wed Oct 6 10:35:15 2021 +0200
    
        Added a simple endpoint for test
    
    commit e330023f8933b2eca553c53ed01f1485c4206040
    Merge: 173d2b5 f788b96
    Author: Christoffer Quakernaat <cph-cq2@cphbusiness.dk>
    Date:   Tue Oct 5 19:23:15 2021 +0200
    
        Merge branch 'main' of github.com:Incognito95/CA-1-Front-End into Christoffer
    
    commit 173d2b5fe55bd7c7d5902ff2a1762f691dbfda82
    Author: Christoffer Quakernaat <cph-cq2@cphbusiness.dk>
    Date:   Tue Oct 5 18:10:45 2021 +0200
    
        first commit

--- src/main/java/rest/RenameMeResource.java
+++ src/main/java/rest/RenameMeResource.java
@@ -11,11 +11,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 //Todo Remove or change relevant parts before ACTUAL use
-@Path("xxx")
+@Path("Persons")
 public class RenameMeResource {
 
-    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
-       
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(); 
     private static final MainFacade FACADE =  MainFacade.getFacadeExample(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
             
@@ -25,13 +24,21 @@ public class RenameMeResource {
         return "{\"msg\":\"Hello World\"}";
     }
     
-    @Path("count")
+    @Path("firstName")
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public String getRenameMeCount() {
-        
-        
-        //System.out.println("--------------->"+count);
-        return "{\"count\":"+"}";  //Done manually so no need for a DTO
+    public String firtName() {
+        return "{\"msg\":\"Firstname says what\"}"; //How do we get a person's name in here my boys?
     }
+    
+    
+//    @Path("count")
+//    @GET
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public String getRenameMeCount() {
+//        
+//        
+//        //System.out.println("--------------->"+count);
+//        return "{\"count\":"+"}";  //Done manually so no need for a DTO
+//    }
 }
