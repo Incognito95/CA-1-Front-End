3657ead5de61408355b299e41f17c3bac2b3723a
Ermin Dzafic <71074936+ErminDZ@users.noreply.github.com>
2021-10-06 18:24:22 +0200

    added getallpersonsbycityorzip and editperson

--- src/main/java/facades/MainFacade.java
+++ src/main/java/facades/MainFacade.java
@@ -2,7 +2,15 @@ package facades;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import dtos.PersonDTO;
+import entities.Person;
 import utils.EMF_Creator;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainFacade {
@@ -29,34 +37,71 @@ public class MainFacade {
         return emf.createEntityManager();
     }
 
-//    public PersonDTO CreatePerson() {
-//        EntityManager em = getEntityManager();
-//        try {
-//            em.getTransaction().begin();
-//            Query q = em.createQuery("INSERT INTO PERSON SET FIRSTNAME = 'admin', LASTNAME = 'admin', EMAIL = 'admin@admin.com'");
-//
-//            int createPerson = q.executeUpdate();
-//            System.out.println("-------------------------------------------------");
-//            System.out.println("You have inserted: " + createPerson);
-//
-//            em.getTransaction().commit();
-//        } finally {
-//            em.close();
-//        }
-//        return new PersonDTO();
-//    }
+    public PersonDTO CreatePerson() {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            Query q = em.createQuery("update Person p SET p.firstName =:firstname, p.lastName =:lastname, p.email =:email");
 
-//    public PersonDTO getById(long id) { //throws RenameMeNotFoundException {
-//        EntityManager em = emf.createEntityManager();
-//        Person rm = em.find(Person.class, id);
-//        if (rm == null)
-//            throw new RenameMeNotFoundException("The RenameMe entity with ID: "+id+" Was not found");
-//        return new PersonDTO(rm);
-//    }
+            int createPerson = q.executeUpdate();
+            System.out.println("-------------------------------------------------");
+            System.out.println("You have inserted: " + createPerson);
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+        return new PersonDTO();
+    }
+
+    public PersonDTO getById(long id) { //throws RenameMeNotFoundException {
+        EntityManager em = emf.createEntityManager();
+        Person rm = em.find(Person.class, id);
+        if (rm == null)
+           // throw new RenameMeNotFoundException("The RenameMe entity with ID: "+id+" Was not found");
+        return new PersonDTO(rm);
+        return null;
+    }
+
+    public PersonDTO getAllPersonsByCiytOrZip()
+    {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Query q = em.createNativeQuery("");
+            List<Object[]> getAllPersonsByCiytOrZip = q.getResultList();
+            System.out.println("-------------------------------------");
+            for (Object[] a : getAllPersonsByCiytOrZip)
+                System.out.println(Arrays.toString(a));
+            {
+                em.getTransaction().commit();
+            }
+        }finally {
+            em.close();
+        }
+        return new PersonDTO();
+    }
+
+    public PersonDTO editPerson()
+    {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Query q = em.createNativeQuery("");
+            List<Object[]> editPerson = q.getResultList();
+            System.out.println("-------------------------------------");
+            for (Object[] a : editPerson)
+                System.out.println(Arrays.toString(a));
+            {
+                em.getTransaction().commit();
+            }
+        }finally {
+            em.close();
+        }
+        return new PersonDTO();
+    }
 
 //    public List<PersonDTO> getAll(){
 //        EntityManager em = emf.createEntityManager();
-//        TypedQuery<Person> query = em.createQuery("SELECT r FROM RenameMe r", Person.class);
+//        TypedQuery<Person> query = em.createQuery("SELECT p FROM Person p", Person.class);
 //        List<Person> rms = query.getResultList();
 //        return PersonDTO.getDtos(rms);
 //    }
