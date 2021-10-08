b38abd8d7603499bffd82e24e3f651d609da4868
ChristofferQ <71074785+ChristofferQ@users.noreply.github.com>
2021-10-08 11:10:33 +0200

    Missed one

--- src/main/java/dtos/PhoneDTO.java
+++ src/main/java/dtos/PhoneDTO.java
@@ -1,23 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dtos;
 
 import entities.Phone;
 
-/**
- *
- * @author Christoffer
- */
 public class PhoneDTO {
 
     private int number;
     private String description;
 
-    public PhoneDTO() {
-    }
+    public PhoneDTO() {}
 
     public PhoneDTO(Phone phone) {
         this.number = phone.getNumber();
